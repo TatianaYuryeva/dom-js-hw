4fae039ae63b648ef44ba2e3ec20768bffca40a0
Tatiana Yuryeva <yuryevat@list.ru>
2023-04-29 13:51:12 +0200

    appveyor updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 function createGame() {
-  const goblin = document.createElement('div');
-  goblin.classList.add('goblin')
+  const goblin = document.createElement('img');
+  goblin.setAttribute('src', 'img/goblin.png')
 
   const getHole = (index) => document.querySelector(`#hole${index}`);
 
@@ -9,11 +9,25 @@ function createGame() {
   const holeInterval = setInterval(() => {
     const activeHoleIndex = Math.floor(1 + Math.random() * 16);
     activateHole(activeHoleIndex);
-  }, 1500);
+  }, 10500);
 }
+
+// function createGame() {
+//   const goblin = document.createElement('div');
+//   goblin.classList.add('goblin')
+
+//   const getHole = (index) => document.querySelector(`#hole${index}`);
+
+//   const activateHole = (index) => getHole(index).append(goblin);
+
+//   const holeInterval = setInterval(() => {
+//     const activeHoleIndex = Math.floor(1 + Math.random() * 16);
+//     activateHole(activeHoleIndex);
+//   }, 1500);
+// }
 
 document.addEventListener("DOMContentLoaded", createGame)
 
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
+// export default function demo(value) {
+//   return `Demo: ${value}`;
+// }
